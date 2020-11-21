d2ce736626ce7fe7b009fb19ff7ec57d5c483902
Theo Wan <theowan816@yahoo.com>
2020-11-21 03:14:11 -0600

    Adding menu for navigating to different game - Theo

---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void ChooseSampleGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ChooseTheoGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ChooseWeiGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ChooseKevinGame()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void ExitTheGame()
+    {
+        Debug.Log("EXITING GAME");
+        Application.Quit();
+    }
+}
